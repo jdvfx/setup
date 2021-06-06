6d270a4c6cf06082adb60f857d897174346696c0
bunker <bunker@bunker2.hitronhub.home>
2021-06-05 21:36:50 -0700

    install curl first and loop through packages to install with -y

--- linux_install.sh
+++ linux_install.sh
@@ -1,4 +1,5 @@
-# after Xubuntu minimal install
+#
+
 
 # get rid of snap
 echo ">>> removing SNAP!"
@@ -10,12 +11,17 @@ sudo apt purge snapd
 
 echo ">>> adding SPOTIFY repo"
 sleep 2
+sudo apt-get install curl -y
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-echo -e ">>> installing packages: \ngit firefox htop feh curl neovim xmonad \nlibghc-xmonad-contrib-dev dmenu build-essential \ngnome-disk-utility xmobar scrot spotify-client autoconf libssl-dev"
+INSTALL_PKGS="git firefox htop feh curl neovim xmonad libghc-xmonad-contrib-dev dmenu build-essential gnome-disk-utility xmobar scrot spotify-client autoconf libssl-dev"
+echo -e ">>>installing packages:"
+echo $INSTALL_PKGS
 sleep 2
-sudo apt install git firefox htop feh curl neovim xmonad libghc-xmonad-contrib-dev dmenu build-essential gnome-disk-utility xmobar scrot spotify-client autoconf libssl-dev
+for i in $INSTALL_PKGS; do
+  sudo apt-get install -y $i
+done
 
 echo -e ">>> GIT clone \nSuckless ST, Bunker dotfiles & Autoconf"
 sleep 2
@@ -25,8 +31,9 @@ git clone https://github.com/jdvfx/dotfiles.git
 git clone https://github.com/jdvfx/autoconf-ubu20.04.git
 git clone http://git.suckless.org/st
 
-#logout and log back into xmonad (default shortcut is Alt+shift+Enter)
 
+
+#logout and log back into xmonad (default shortcut is Alt+shift+Enter)
 
 #copy .bashrc
 
