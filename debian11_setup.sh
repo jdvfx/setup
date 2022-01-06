8c4a6d9c789c1f86a8824c55281c5aa20f31bdc6
Julien Depredurand <julien.depredurand@gmail.com>
2022-01-05 20:25:02 -0800

    added debian setup script: add non_free, update, upgrade, install git

--- debian11_setup.sh
+++ debian11_setup.sh
@@ -1,6 +1,17 @@
-deb http://deb.debian.org/debian bullseye main contrib non-free
+echo 'deb http://deb.debian.org/debian bullseye main contrib non-free
 deb-src http://deb.debian.org/debian bullseye main contrib non-free
 deb http://deb.debian.org/debian-security bullseye/updates main contrib non-free
 deb-src http://deb.debian.org/debian-security bullseye/updates main contrib non-free
 deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
+deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free' > sources.list
+
+su
+cp /etc/apt/source.list /etc/apt/source.list_backup
+cp source.list /etc/apt/source.list
+
+apt update
+apt upgrade
+apt install git
+
+
+
