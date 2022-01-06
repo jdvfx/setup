303ee3c8c5a8ce418481a37e9808ea3d4cc6af10
Julien Depredurand <julien.depredurand@gmail.com>
2022-01-05 20:27:43 -0800

    sudo first

--- debian11_setup.sh
+++ debian11_setup.sh
@@ -1,3 +1,5 @@
+su
+
 echo 'deb http://deb.debian.org/debian bullseye main contrib non-free
 deb-src http://deb.debian.org/debian bullseye main contrib non-free
 deb http://deb.debian.org/debian-security bullseye/updates main contrib non-free
@@ -5,7 +7,6 @@ deb-src http://deb.debian.org/debian-security bullseye/updates main contrib non-
 deb http://deb.debian.org/debian bullseye-updates main contrib non-free
 deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free' > sources.list
 
-su
 cp /etc/apt/source.list /etc/apt/source.list_backup
 cp source.list /etc/apt/source.list
 
